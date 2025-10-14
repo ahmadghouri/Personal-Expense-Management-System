23e652fdbc7ce4ac425b27b75c114de11aad1078
Muhammad Ahmad <ahmadghouriweb@gmail.com>
2025-10-14 23:41:10 +0500

    fixed protected routs

--- frontend/src/components/useAuth.js
+++ frontend/src/components/useAuth.js
@@ -1,7 +1,8 @@
-import { ref } from 'vue';
-import api from '../Api/AxiosBase';
+import { ref } from "vue";
+import api from "../Api/AxiosBase";
 
 const user = ref(null);
+const role = ref(localStorage.getItem("user_role") || null);
 const loading = ref(true);
 const error = ref(null);
 
@@ -9,18 +10,22 @@ export default function useAuth() {
   const fetchUser = async () => {
     try {
       loading.value = true;
-      const res = await api.get('profile');
+      const res = await api.get("profile");
       user.value = res.data.user;
-      localStorage.setItem('user', JSON.stringify(user.value)); 
+      role.value = res.data.user.role;
+
+      localStorage.setItem("user", JSON.stringify(user.value));
+      localStorage.setItem("user_role", role.value);
     } catch (err) {
       error.value = err;
       user.value = null;
-      localStorage.removeItem('user'); 
-      console.error(err);
+      role.value = null;
+      localStorage.removeItem("user");
+      localStorage.removeItem("user_role");
     } finally {
       loading.value = false;
     }
   };
 
-  return { user, loading, error, fetchUser };
+  return { user, role, loading, error, fetchUser };
 }
